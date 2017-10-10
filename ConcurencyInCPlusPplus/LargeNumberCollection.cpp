51216b12d3e47f3b07f316342575df461e0710ee
Aleksander Naumenok <aleksander.naumenok@gmail.com>
2017-10-10 15:37:30 +0200

    no message

---
+++ ConcurencyInCPlusPplus/LargeNumberCollection.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "LargeNumberCollection.h"
+
+
+LargeNumberCollection::LargeNumberCollection()
+{
+}
+
+
+LargeNumberCollection::~LargeNumberCollection()
+{
+}
---
+++ ConcurencyInCPlusPplus/LargeNumberCollection.h
@@ -0,0 +1,8 @@
+#pragma once
+class LargeNumberCollection
+{
+public:
+	LargeNumberCollection();
+	virtual ~LargeNumberCollection();
+};
+
